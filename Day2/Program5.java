2e6933a52e7b418567d78c0016fd53b88b80dd68
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:52 +0530

    Create Program5.java

---
+++ Day2/Program5.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class Program5 {
+    public static void main(String[] args){
+        boolean b = true;
+        int n =10;
+        double n1 = n;
+        double n3= 16.47;
+        int n4 =(int) n3;
+         System.out.println(n4);
+         System.out.println(n1);
+    }
+}
