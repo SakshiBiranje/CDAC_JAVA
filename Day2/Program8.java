b334650cdf439b52d003439d4f14705b85818b46
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:09:02 +0530

    Create Program8.java

---
+++ Day2/Program8.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class Program8 {
+    public static void main(String[] args) {
+        Integer l = new Integer(10);
+        int n = l;
+        int n2 = l.intValue();
+        System.out.println(n);
+        System.out.println(n2);
+    }
+}
