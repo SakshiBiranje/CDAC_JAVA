26a3585c94f17c156dfe0459782074a735380197
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:55 +0530

    Create Program6.java

---
+++ Day2/Program6.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class Program6 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        Short a = (short) ch;
+        System.out.println(a);
+        char ch1 = 99;
+        char a1 = (char) ch1;
+        System.out.println(ch1);
+    }
+}
