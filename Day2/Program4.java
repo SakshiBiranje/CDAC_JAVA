1f6cbbb9fa1f2fcc1dc826cea1dbd7943d2157f9
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:49 +0530

    Create Program4.java

---
+++ Day2/Program4.java
@@ -0,0 +1,16 @@
+package Day2;
+
+import java.util.Scanner;
+public class Program4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name : ");
+        String name = sc.nextLine();
+        System.out.print("Enter age : ");
+        int age = sc.nextInt();
+        System.out.println("Name : "+ name);
+        System.out.println("Age : "+ age);
+
+    }
+}
