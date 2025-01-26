2bd694840a8763628bcc3dae856294a8bf9dffc8
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:59 +0530

    Create Program7.java

---
+++ Day2/Program7.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class Program7 {
+    public static void main(String[] args) {
+        int n = 10;
+        Integer i = (Integer)n;
+        Integer i2= n;
+        System.out.println(i2);
+    }
+    
+}
