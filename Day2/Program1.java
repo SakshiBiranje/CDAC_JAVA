d289bb92dc732932814c60270deb003652d88295
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:41 +0530

    Create Program1.java

---
+++ Day2/Program1.java
@@ -0,0 +1,25 @@
+package Day2;
+class One{
+    public static void main(String[] args){
+        System.out.println("Hello from class One main");
+    }
+}
+class Two{
+    public static void main(String[] args){
+        System.out.println("Hello from class Two main");
+    }
+}
+class Three{
+    public static void main(String[] args){
+        System.out.println("Hello from class Three main");
+    }
+}
+
+public class Program1{
+    public static void main(int n){
+        System.out.println("Hello from class main(int n)");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello from class main");
+    }
+}
