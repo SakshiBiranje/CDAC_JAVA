a77c4f876654365e70468342efa5b16f05abb5b9
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-26 22:08:44 +0530

    Create Program2.java

---
+++ Day2/Program2.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Program2 {
+    
+    public static void main(int n){
+        System.out.println("Hello from class main(int n)");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello from class main");
+        main(10);
+       
+    }
+}
+
