03f7861964c2c77956fb1172b85ce8ed724b60a6
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-27 21:12:27 +0530

    Create program01.java

---
+++ Day4/Demo01/src/com/sakshi/program01.java
@@ -0,0 +1,11 @@
+package com.sakshi;
+//import com.sunbeam.demo.p2.Time;
+public class program01 {
+
+	public static void main(String[] args) {
+		Time t1 = new Time();
+		t1.accept();
+		t1.display();
+	}
+
+}
