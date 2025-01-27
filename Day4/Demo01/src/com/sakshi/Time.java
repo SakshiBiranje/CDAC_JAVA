9057a7087074b774062bf4b7d6f281237e3aca6d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-27 21:12:30 +0530

    Create Time.java

---
+++ Day4/Demo01/src/com/sakshi/Time.java
@@ -0,0 +1,14 @@
+package com.sakshi;
+
+public class Time {
+	int hr;
+	int min;
+
+	public void accept() {
+		System.out.println("Time::accept()");
+	}
+
+	public void display() {
+		System.out.println("Time::display()");
+}
+}
