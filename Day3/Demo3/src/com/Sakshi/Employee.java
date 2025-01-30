de7fe02db056deec8847205bc4929cffb2d1934e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-30 19:05:58 +0530

    Create Employee.java

---
+++ Day3/Demo3/src/com/Sakshi/Employee.java
@@ -0,0 +1,14 @@
+package com.Sakshi;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		public void accept() {
+			Test t1 = new Test();
+			// System.out.println(t1.num1);// NOT OK
+			// System.out.println(t1.num2);// NOT OK
+			// System.out.println(t1.num3);// NOT OK
+			System.out.println(t1.num4);
+	}
+
+}
