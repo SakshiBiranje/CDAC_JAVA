6c099a29c78d7466c717c87bda2e18eb1c0f3058
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-30 19:06:23 +0530

    Create TstDerived.java

---
+++ Day3/Demo3/src/com/Sakshi/TstDerived.java
@@ -0,0 +1,12 @@
+package com.Sakshi;
+
+public class TestDerived extends Test {
+
+	public void display() {
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+
+}
